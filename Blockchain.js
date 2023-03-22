9852988171e45dfb66d836f7cc35cc15f71fd65e
dinesh-jaysiri <ywdinesh95@gmail.com>
2023-03-22 11:44:16 +0530

    add files

---
+++ Blockchain.js
@@ -0,0 +1,48 @@
+//-------------------------  01. what is blockchain? ------------------------------------------
+
+// Blockchain is a distributed, decentralized digital ledger that allows for secure and transparent transactions without the need for a central authority. It is essentially a chain of blocks that contains information about transactions, and each block is linked to the previous block in the chain, creating a permanent and tamper-proof record of all transactions on the network.
+
+//----------------------  02. What is the purpose of blockchain -------------------------------
+
+// with a blockchain we can decntralize where code runs and agree on the output
+/// there is no single owner of the code's Execution
+//// the code always runs as porgrmmed
+//// the code is trnsparently varifiable
+
+//----------------------  03. What is the smart contract blockchain -------------------------------
+
+// Smart contract blockchains, such as Ethereum, provide developers with a decentralized platform for executing code. The code of a smart contract is stored on the blockchain and is executed automatically when certain conditions are met.
+
+//Additionally, smart contract blockchains are transparent, as every transaction on the blockchain is publicly visible and can be verified by anyone. This makes it possible for users to trust the system without having to rely on a central authority.
+
+//Overall, smart contract blockchains provide a powerful platform for developers to create decentralized applications that can be used in a variety of industries, including finance, supply chain management, and healthcare, among others.
+
+//----------------------  04. What is the bitcoin -------------------------------
+
+// Bitcoin was the first successful blockchain and cryptocurrency
+// but there was much resarch and many attemps before it
+// Most of the components were discoverd alrady
+
+//main compoinent of bitcoin network
+/// Proof of Work - Security
+/// Mining Rewards - Financial Incentives
+/// Public Key Cryptography - Authentication
+/// Linked Data Structure - Chronology
+/// Peer to Peer Network - Permissionless
+
+//----------------------  05. What is the Cryptographic Hash Functions -------------------------------
+
+//Cryptographic hash functions are mathematical algorithms that take in input data of any length and output a fixed-size string of characters, known as a hash
+
+//----------------------  06. What is the specific properties of cryptographic hash functions-------------------------------
+
+/// Deterministic - One specific input always maps to the same specific output
+///  Pseudorandom - It is not possible to guess the output based on the output of similar inputs
+/// If someone gives you a new output, you could not determine an input without guessing
+/// Fast to Compute - It must be a quick calculation for a computer
+/// Collision-resistant - The chance of a collision should be infinitesimally small
+
+//----------------------  07. What is the Cryptographic Hash Functions -------------------------------
+//----------------------  08. What is the Cryptographic Hash Functions -------------------------------
+//----------------------  09. What is the Cryptographic Hash Functions -------------------------------
+//----------------------  10. What is the Cryptographic Hash Functions -------------------------------
---
+++ Hashing.js
@@ -0,0 +1,2 @@
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
